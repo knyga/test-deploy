646488f41d31ddb4b43fb7e6d24cd4d6ea252699
Oleksandr Knyga <oleksandrknyga@gmail.com>
2015-08-26 11:30:00 +0200

    update deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-PULLRESULT=git pull origin master
-echo $PULLRESULT
+PULLRESULT=`git pull origin master`
+if [ "$PULLRESULT" == "Already up-to-date." ]
+  then
+    echo "up to date"
+  else
+    echo "not up to date"
+  fi
